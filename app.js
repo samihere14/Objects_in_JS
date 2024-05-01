605a79eaaae8850a8c3954bb28cf6ea62a922183
Muhammad Sami <134046729+samihere14@users.noreply.github.com>
2024-05-01 18:05:50 +0500

    Objects in JavaScript.

---
+++ app.js
@@ -0,0 +1,45 @@
+//  Anonymus Function
+
+// var func = function(){
+//     console.log("anonymus function call..")
+// }
+// func()
+
+// var studentObj = {
+//     name: "M Sami",
+//     age: 20,
+//     education:{
+//         courses: "html, css, js",
+//         year: 2022,
+//     }
+
+// }
+// console.log(studentObj.education);
+// console.log(typeof studentObj);
+
+//  studentobj.courses = ["python", "c"];
+
+// ================================== Console is an object and log is its method ================================================
+//======================================  Alert is an Built-in function =========================================================
+
+
+// var arr = ["M Sami", 20, true];
+
+// console.log(arr);
+
+//  arr.push("Hello there");
+
+// console.log(arr);
+
+// studentobj.courses.push("reactjs");
+// console.log(studentobj.courses);
+
+// in
+
+// console.log("name" in studentobj);
+
+// // delete
+
+// delete studentobj.courses;
+
+// console.log(studentobj);
